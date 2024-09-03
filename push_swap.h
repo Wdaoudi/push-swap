1864d1e63dd84259c6fc939efbe19c51a5c9744b
Wassim Daoudi Idrissi <wdaoudi-@made-f0Ar9s10.clusters.42paris.fr>
2024-09-03 20:09:51 +0200

    kk

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,12 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:43:34 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/09/03 18:54:48 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/09/03 19:33:12 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
 
 # include <limits.h>
 # include <stdio.h>
@@ -29,31 +28,26 @@ typedef struct s_list
 	int				value;
 	int				index;
 	struct s_list	*next;
-}				t_list;
-//voir si il est necessaire de definir une structure stack 
-typedef struct stack
-{
-    
-}t_stack;
+	struct s_list	*prev;
+}					t_list;
 
-//temporaire
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-char	**ft_split(char const *s, char c);
-char	**slip(char const *s, char c, char **str);
-int	lenght_word(char const *s, char c, int i);
-int	count_words(char const *s, char c);
+// temporaire
+size_t				ft_strlen(const char *str);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strdup(const char *s);
+char				**ft_split(char const *s, char c);
+char				**slip(char const *s, char c, char **str);
+int					lenght_word(char const *s, char c, int i);
+int					count_words(char const *s, char c);
 
-//fonction utils
-long	ft_atoi(const char *nptr);
-char	*ft_strcat(char *dest, const char *src);
+// fonction utils
+long				ft_atoi(const char *nptr);
+char				*ft_strcat(char *dest, const char *src);
 
-//fonction de parsing/check
-char	**parsing(char **str, int ac);
-int	check_error(char *str);
-int	check_lim(int nbr);
-int	check(char **str, int ac);
-
+// fonction de parsing/check
+char				**parsing(char **str, int ac);
+int					check_error(char *str);
+int					check_lim(int nbr);
+int					check(char **str, int ac);
 
 #endif
--- utils.c
+++ utils.c
@@ -6,9 +6,52 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 16:22:54 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/09/02 16:23:12 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/09/03 19:11:34 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+char	*ft_strcat(char *dest, const char *src)
+{
+	char	*ptr;
+
+	ptr = dest;
+	while (*ptr != '\0')
+		ptr++;
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
+long	ft_atoi(const char *nptr)
+{
+	long	nbr;
+	long	i;
+	int		signe;
+
+	i = 0;
+	signe = 1;
+	nbr = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == 45)
+	{
+		signe = -signe;
+		i++;
+	}
+	else if (nptr[i] == 43)
+		i++;
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		nbr = (nbr * 10) + nptr[i] - '0';
+		i++;
+	}
+	if ((nbr * signe >= 2147483647) || (nbr * signe <= -2147483648))
+		return (printf("trop grand int\n"), nbr + nbr);
+	return (nbr * signe);
+}
