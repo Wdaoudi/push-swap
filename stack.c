2cf0898ee97cdb6d5a31b28173e94044107ab2eb
Wassim Daoudi Idrissi <wdaoudi-@made-f0Ar10s8.clusters.42paris.fr>
2024-09-05 19:40:35 +0200

    kk

--- stack.c
+++ stack.c
@@ -6,40 +6,82 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:44:02 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/09/05 18:08:21 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/09/05 19:37:49 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+// #include "push_swap.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 // implementer les focntions push, pop, isempty, top
 
-t_stack	init_stack(int *tab, int size)
+typedef struct s_stack
+{
+	struct s_list	*first;
+}					t_stack;
+
+typedef struct s_list
+{
+	int				content;
+	int				index;
+	int				cost;
+	struct s_list	*target;
+	struct s_list	*next;
+	struct s_list	*prev;
+}					t_list;
+
+t_list	*ft_lstnew(int content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+t_stack	*init_stack(int *tab, int size)
 {
 	int		i;
 	t_stack	*stack;
 	t_list	*current;
 
-	i = 0;
+	i = 1;
 	stack = malloc(sizeof(t_stack));
 	if (!stack)
 		return (NULL);
-	current = ft_lstnew(tab[i++]); //current vaut lstnew du tab de i ++
-	stack->first = current;//le premier nnoeud de la liste vaut current
+	current = ft_lstnew(tab[0]); // current vaut lstnew du tab de i ++
+	stack->first = current;      // le premier nnoeud de la liste vaut current
 	while (i < size)
 	{
 		current->next = ft_lstnew(tab[i++]);
-		current->next->prev = current; //car le pointeur previous du next de current point vers current
-		current = current->next; // current deviens le maillon d apres on fait avancer de un 
+		current->next->prev = current;
+		// car le pointeur previous du next de current point vers current
+		current = current->next;
+		// current deviens le maillon d apres on fait avancer de un
 	}
 	return (stack);
-}
-t_list	new_node(int content)
-{
-	ft_lstnew(content);
 }
 
 int	main(int ac, char **av)
 {
-	stack(atoi());
+	int	size;
+	int	*arr;
+
+	size = ac - 1;
+	arr = malloc(size * sizeof(int));
+	if (!arr)
+		return (1);
+	for (int i = 0; i < size; i++)
+		arr[i] = atoi(av[i + 1]);
+	init_stack(arr, ac - 1);
+	for (int i = 0; i < size; i++)
+		printf("%d\n", arr[i]);
+	return (0);
 }
