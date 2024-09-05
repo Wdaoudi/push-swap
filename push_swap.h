ed1033f6370c9619b5355e48d91e0acab128349f
Wassim Daoudi Idrissi <wdaoudi-@made-f0Ar10s8.clusters.42paris.fr>
2024-09-05 16:38:15 +0200

    kk

--- push_swap.h
+++ push_swap.h
@@ -6,30 +6,36 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:43:34 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/09/04 19:26:25 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/09/05 15:35:59 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "./libft/libft.h"
+# include "./printf/ft_printf.h"
 # include <limits.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
 
-# include "./libft/libft.h"
-# include "./printf/ft_printf.h"
-
 // definition de la structure du node
-typedef struct t_list
+typedef struct s_list
 {
-	int				value;
+	int				content;
 	int				index;
-	struct t_list	*next;
-	struct t_list	*prev;
-}					u_list;
+	int				cost;
+	struct s_list	*target;
+	struct s_list	*next;
+	struct s_list	*prev;
+}					t_list;
+
+typedef struct s_stack
+{
+	struct s_list	*first;
+}					t_stack;
 
 // temporaire
 // size_t				ft_strlen(const char *str);
@@ -50,10 +56,10 @@ int					check_error(char *str);
 int					check_lim(int nbr);
 int					check(char **str, int ac);
 
-//test
-int	check(char **str, int ac);
-int	parse_and_check(char **str, int ac);
-int	check_limits(char **parsed);
-int	free_parsed(char **parsed);
+// test
+int					check(char **str, int ac);
+int					parse_and_check(char **str, int ac);
+int					check_limits(char **parsed);
+int					free_parsed(char **parsed);
 
 #endif
